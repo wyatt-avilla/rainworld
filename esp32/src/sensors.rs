5d43c257e9129918441e708ed6053c555a411ff1
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-07-26 14:42:17 -0700

    add moisture sensor read function

--- esp32/src/sensors.rs
+++ esp32/src/sensors.rs
@@ -1,6 +1,34 @@
-use esp_idf_hal::io::{EspIOError, Write};
+use esp_idf_hal::{
+    adc::{
+        attenuation::DB_11,
+        oneshot::{config::AdcChannelConfig, AdcChannelDriver, AdcDriver},
+        ADC1,
+    },
+    gpio::{ADCPin, Gpio32},
+    io::{EspIOError, Write},
+    peripheral::Peripheral,
+    sys::EspError,
+};
 use esp_idf_svc::http::server::{EspHttpConnection, Request};
 use thiserror::Error;
+
+pub trait MoistureSensorPin {}
+
+impl MoistureSensorPin for Gpio32 {}
+
+pub fn moisture_value_from(
+    adc: &AdcDriver<'_, ADC1>,
+    pin: impl MoistureSensorPin + Peripheral<P = impl ADCPin<Adc = ADC1>>,
+) -> Result<u16, EspError> {
+    let config = AdcChannelConfig {
+        attenuation: DB_11,
+        ..Default::default()
+    };
+
+    let mut adc_pin = AdcChannelDriver::new(adc, pin, &config)?;
+
+    adc.read(&mut adc_pin)
+}
 
 #[derive(Error, Debug)]
 pub enum MockPlantsError {
