80c0517de80b16dffff9dd629b9930f0f82c4774
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-07-30 23:10:19 -0700

    appease clippy

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -19,7 +19,18 @@ async fn main() -> anyhow::Result<()> {
 
     log::info!("Running server on port {}", args.port);
     log::info!("Expecting ESP32 at '{}'", args.esp32_url);
-    log::info!("Expecting database at '{}'", args.influx_db_url);
+    log::info!("Expecting database at '{}'", args.influxdb_url);
+
+    let client = database::Client::new(
+        &args.influxdb_database_name,
+        &args.influxdb_url,
+        &args.influxdb_auth_token_file,
+    )?;
+
+    let test_query = client
+        .query(format!("SELECT * from {}", args.influxdb_database_name).as_str())
+        .await;
+    dbg!(&test_query);
 
     let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", args.port).as_str()).await?;
 
