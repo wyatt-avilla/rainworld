235e1309828e22b14dbd4eff8f273fc10fb7b0c9
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-08-10 16:22:16 -0700

    custom deserialization for `ScientificPlantName`

--- shared/src/plant.rs
+++ shared/src/plant.rs
@@ -28,13 +28,32 @@ pub struct Readings {
     pub moisture_level: u16,
 }
 
+mod literals {
+    pub const FICUS_ELASTICA: &str = "Ficus Elastica";
+    pub const MONSTERA_DELICIOSA: &str = "Monstera Deliciosa";
+    pub const DIEFFENBACHIA_REFLECTOR: &str = "Dieffenbachia Reflector";
+}
+
 // who needs strum anyway
 impl Display for ScientificPlantName {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.write_str(match self {
-            ScientificPlantName::FicusElastica => "Ficus Elastica",
-            ScientificPlantName::MonsteraDeliciosa => "Monstera Deliciosa",
-            ScientificPlantName::DieffenbachiaReflector => "Dieffenbachia Reflector",
+            ScientificPlantName::FicusElastica => literals::FICUS_ELASTICA,
+            ScientificPlantName::MonsteraDeliciosa => literals::MONSTERA_DELICIOSA,
+            ScientificPlantName::DieffenbachiaReflector => literals::DIEFFENBACHIA_REFLECTOR,
         })
+    }
+}
+
+impl TryFrom<&str> for ScientificPlantName {
+    type Error = String;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        match value {
+            literals::FICUS_ELASTICA => Ok(ScientificPlantName::FicusElastica),
+            literals::MONSTERA_DELICIOSA => Ok(ScientificPlantName::MonsteraDeliciosa),
+            literals::DIEFFENBACHIA_REFLECTOR => Ok(ScientificPlantName::DieffenbachiaReflector),
+            _ => Err(String::from("Unknown ScientificPlantName")),
+        }
     }
 }
