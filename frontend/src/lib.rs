5e45b01a7287aa4d4a2c8cfa7a446cd47698132b
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-08-15 18:50:35 -0700

    formatting

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -6,15 +6,15 @@ use wasm_bindgen::prelude::*;
 #[component]
 fn App() -> impl IntoView {
     let styler_class = style! { "App",
-        div {
-            border: 1px solid black;
-            margin: 25px 50px 75px 100px;
-            background-color: lightblue;
-        }
+      div {
+        border: 1px solid black;
+        margin: 25px 50px 75px 100px;
+        background-color: lightblue;
+      }
 
-        div.one {
-            color: red;
-        }
+      div.one {
+        color: red;
+      }
     };
 
     let panic_handler = move |_| {
@@ -24,15 +24,15 @@ fn App() -> impl IntoView {
     let log_handler = move |_| console::log!("Hello console");
 
     view! { class = styler_class,
-        <div class="one">
-          <h1>"Hello Leptos!"</h1>
-          <button on:click=panic_handler>
-            "Click to Panic!"
-          </button>
-          <button on:click=log_handler>
-            "Click to log!"
-          </button>
-        </div>
+      <div class="one">
+        <h1>"Hello Leptos!"</h1>
+        <button on:click=panic_handler>
+          "Click to Panic!"
+        </button>
+        <button on:click=log_handler>
+          "Click to log!"
+        </button>
+      </div>
     }
 }
 
